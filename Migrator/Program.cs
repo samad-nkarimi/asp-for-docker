282efc4d538d67ff6c2343e9530b576c1598d63b
samadk-ubuntu <samad.nkarimi@gmail.com>
2025-07-16 02:11:53 +0330

    Create Migrate console app

---
+++ Migrator/Program.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
+using AspForDocker.AspDbContext;
+using Microsoft.Extensions.Configuration;
+
+var host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices((ctx, services) =>
+    {
+        services.AddDbContext<AppDbContext>(options =>
+            options.UseNpgsql(ctx.Configuration.GetConnectionString("DefaultConnection")));
+    })
+    .Build();
+
+using var scope = host.Services.CreateScope();
+var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+Console.WriteLine("Applying migrations...");
+db.Database.Migrate();
+Console.WriteLine("Done.");
